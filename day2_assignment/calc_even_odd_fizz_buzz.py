3931cdc0e371130e7c3e9b2a991e325676716188
DomWat <domwat20@gmail.com>
2020-09-09 13:49:54 -0500

    day 2 assignment (calculator, even/odd, Fizz Buzz)

---
+++ day2_assignment/calc_even_odd_fizz_buzz.py
@@ -0,0 +1,42 @@
+#calculator
+num1 = int(input("Enter first number: "))
+operand = input("Enter an operand (+, -, *, /): ")
+num2 = int(input("Enter second number: "))
+
+if operand == "+":
+    answer = num1 + num2
+    print(answer)
+elif operand == "-":
+    answer = num1 - num2
+    print(answer)
+elif operand == "*":
+    answer = num1 * num2 
+    print(answer)
+elif operand == "/":
+    answer = num1 / num2
+    print(answer)
+else: 
+    print("Please enter a proper answer!")
+
+#math = int(num1) + operand + int(num2)
+#print(math)
+
+
+#even odd
+num = input("Enter a number: ")
+if int(num) % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+
+# Fizz Buzz
+number = input("Enter a number: ")
+if int(number) % 3 == 0 and int(number) % 5 == 0:
+    print("Fizz Buzz!")
+elif int(number) % 3 == 0 and int(number) % 5 != 0:
+    print("Fizz")
+elif int(number) % 3 != 0 and int(number) % 5 == 0:
+    print("Buzz")
+else: 
+    print("Try another number")
