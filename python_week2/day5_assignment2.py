f5b3c472aee079cb219c73d9acffa4cc7a236f9e
DomWat <domwat20@gmail.com>
2020-09-14 17:54:25 -0500

    bug fix, still incomplete

--- python_week2/day5_assignment2.py
+++ python_week2/day5_assignment2.py
@@ -1,6 +1,5 @@
 master_list = []
-stores_list = []
-items_list = []
+
 
 
 class Grocery_store:
@@ -19,44 +18,37 @@ class Grocery_item:
 def grocery_store_info():
     name_store = input("Name of the store: ")
     address_store = input("Address of the store: ")
-    stores_list.append(Grocery_store(name_store, address_store))
+    master_list.append(Grocery_store(name_store, address_store))
 
 def grocery_item_info():
     name_item = input("Name of the items: ")
     price_item = input("Price of items: ")
     quantity_item = input("Quantity of items: ")
-    items_list.append(Grocery_item(name_item, price_item, quantity_item))
+    master_list.append(Grocery_item(name_item, price_item, quantity_item))
 
 def options():
-    prompt = """
-    1. add store
-    2. add item
-    3. remove item
-    4. view all lists 
-    f. finish
-    Enter an option: 
-    """ 
-    print(prompt)
-    answer = input("")
-    return answer
+    while True: 
+        print("""
+        1. add store
+        2. add item
+        3. remove item
+        4. view all lists 
+        f. finish
+        Enter an option: 
+        """ )
+        answer = input("")
+        if answer == "1":
+            grocery_store_info()
+            #break
+        elif answer == "2":
+            grocery_item_info()
+        elif answer == "3":
+            pass
+        elif answer == "4":
+            pass
 
-response = str(options())
-
-
-while response != "f":
-    if response == "1":
-        grocery_store_info()
-        break
-        #break
-    elif response == "2":
-        grocery_item_info()
-        options()
-    elif response == "3":
-        pass
-    elif response == "4":
-        pass
-    else:
-        break
+options()
+   
 
 
 
